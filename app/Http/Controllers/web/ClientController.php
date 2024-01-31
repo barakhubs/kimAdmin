69c068e201e9eb3ccb9f84271e7b540a7fe69296
barakhubs <markbrightbaraka22@gmail.com>
2024-01-31 22:27:42 +0300

    changed client api logic

--- app/Http/Controllers/web/ClientController.php
+++ app/Http/Controllers/web/ClientController.php
@@ -9,7 +9,7 @@ use App\Models\Client;
 class ClientController extends Controller
 {
     public function index () {
-        $clients = Client::where('is_parent', '0')->get();
+        $clients = Client::get();
 
         return response()->json(['rows'=>$clients->count(), 'clients' => $clients], 200);
     }
