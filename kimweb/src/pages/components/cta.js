ec9438951c5ee46e01b4c5c4bb975b04e1179ab6
barakhubs <markbrightbaraka22@gmail.com>
2024-02-01 08:35:53 +0300

    added clients

--- kimweb/src/pages/components/cta.js
+++ kimweb/src/pages/components/cta.js
@@ -1,32 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
+import axios, { ASSET_URL } from "../../config/axios";
 
 const Cta = () => {
-    const partnersData = [
-        {
-            id: 1,
-            imgSrc: "images/clients/1.png",
-            text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
-            link: "#",
-        },
-        {
-            id: 2,
-            imgSrc: "images/clients/2.png",
-            text: "The master-builder of human happiness one avoids pleasure itself, because it is pleasure, but because those who do not know pursue.",
-            link: "#",
-        },
-        // Add more partner data as needed
-    ];
+    const [partnersData, setPartnersData] = useState([]);
+
+    useEffect(() => {
+        // Fetch data from the API endpoint /clients
+        axios.get("/clients")
+            .then(response => {
+                // Update the state with the fetched data
+                setPartnersData(response.data.clients);
+            })
+            .catch(error => {
+                console.error("Error fetching data from API:", error);
+            });
+    }, []); // Empty dependency array ensures the effect runs once on mount
 
     const settings = {
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 2, // Display two columns per slide
-        slidesToScroll: 2, // Scroll two columns at a time
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 5000,
         prevArrow: false,
         nextArrow: false,
+        responsive: [
+            {
+                breakpoint: 768, // Breakpoint for mobile devices
+                settings: {
+                    slidesToShow: 1.2,
+                    slidesToScroll: 1.2,
+                },
+            },
+        ],
     };
+
 
     return (
         <section className="partners-section">
@@ -34,28 +45,26 @@ const Cta = () => {
                 <div className="row clearfix">
                     <div className="title-column col-md-4 col-sm-12 col-xs-12">
                         <div className="inner-column">
-                            <h2>Our Partners</h2>
+                            <h2>Our Clients</h2>
                             <div className="text">
-                                Must explain to you how all this mistaken
-                                idea of denouncing pleasure and praising
-                                pain was complete account of the system.
+                                We value the trust and partnership of our clients. Join our growing community of satisfied clients who have experienced success with our services and solutions.
                             </div>
                             <a href="#" className="theme-btn btn-style-one">
-                                Join With Us
+                                Join us now
                             </a>
                         </div>
                     </div>
 
                     <div className="partners-column col-md-8 col-sm-12 col-xs-12">
                         <Slider {...settings}>
-                            {partnersData.map((partner) => (
+                            {partnersData.map(partner => (
                                 <div key={partner.id} className="partner-block">
                                     <div className="inner-box">
                                         <div className="client-img">
-                                            <img src={partner.imgSrc} alt="" />
+                                            <img src={ASSET_URL + partner.logo} alt={partner.name} style={{ width: "180px", height: "120px" }} />
                                         </div>
-                                        <div className="text">{partner.text}</div>
-                                        <a className="read-more" href={partner.link}>
+                                        <div className="text">{partner.name}</div>
+                                        <a className="read-more" href={'https://' + partner.url} target="_blank" rel="noopener noreferrer">
                                             <span className="icon flaticon-right-arrow-1"></span>{" "}
                                             Visit Website
                                         </a>
